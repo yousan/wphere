077913cdf5db60baf0261f4a9a8344ce88b5dad2
Yousan_O <yousan@l2tp.org>
2016-11-13 12:05:33 +0900

    adding test script

--- bin/test.sh
+++ bin/test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
-TESTDIR=~/public_html/example.dev
-mkdir -p ${TESTDIR}
-cd ${TESTDIR}
+set -ex
 
+TESTDIR=~/public_html/example.dev
+
+mkdir -p ${TESTDIR}
+cd -- "$(dirname "$BASH_SOURCE")"
+WPHERE=`pwd -P`/../wphere
+cp .env.test ${TESTDIR}/.env
+cd ${TESTDIR}
+$WPHERE
